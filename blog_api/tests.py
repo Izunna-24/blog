9a46b1a0be2dc1301cf75867e31ad1578fc6b63f
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-11-24 09:46:30 +0100

    cors orgin connection done

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class PostTestCases(APITestCase):
+
+      def test_view_post(self):
+            url = reverse('blog_api:listcreate') #takes the endpoint
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+      def test_create_post(self):
+            self.test_category = Category.objects.create(name='Test Category')
+            self.test_user1 = User.objects.create_user(username='test_user1', password='<PASSWORD>')
+            data = {'title': 'test_title','author': 1, 'excerpt': 'test_excerpt', 'content': 'test_content'}
+            url = reverse('blog_api:listcreate') #takes the endpoint
+            response = self.client.post(url, data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+
+
